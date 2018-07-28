b94e7b0969d4a80702717c2f1833cec28c655aa7
Michal Malý <madcatxster@gmail.com>
2018-07-28 02:33:25 +0200

    Use software name from softwareinfo in About dialog

--- ui/aboutdialog.py
+++ ui/aboutdialog.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QDialog
 import forms.aboutdialog
-import softwareinfo
+from softwareinfo import SoftwareInfo
 
 ECHMET_LINK = 'http://echmet.natur.cuni.cz'
 
@@ -8,9 +8,10 @@ class AboutDialog(QDialog, forms.aboutdialog.Ui_AboutDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
-        self.ql_versionTag.setText(softwareinfo.SoftwareInfo.versionString())
+        self.ql_swName.setText(SoftwareInfo.softwareName())
+        self.ql_versionTag.setText(SoftwareInfo.versionString())
         self.ql_echmetLogo.setText('')
-        self.ql_echmetLogo.setPixmap(softwareinfo.SoftwareInfo.echmetLogo().scaledToWidth(100))
+        self.ql_echmetLogo.setPixmap(SoftwareInfo.echmetLogo().scaledToWidth(100))
 
         self.ql_echmetLink.setText('<a href="{0}">{0}</a>'.format(ECHMET_LINK))
         self.ql_echmetLink.setOpenExternalLinks(True)
